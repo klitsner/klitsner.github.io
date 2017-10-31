10246f419d9dad1200c3f48080e4ea1fb8605fd1
Samson Klitsner <sklitsner@gmail.com>
2017-10-31 12:14:12 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -109,7 +109,8 @@ var projects = [
         medium: 'Music Video',
         description: '',
         thumbnail:'',
-        body:'<p class="description">A work in progress\n\n My final project for DMA 161: Video. The song, "Martini Scorchazio," was produced and recorded by myself along with fellow DMA and Fine Art  <a href ="https://soundcloud.com/shadow-realm-dice-boys">ShadowRealmDiceBoys</a>.\n\nDirector/Editor: Sam Klitsner\nDP: Ky Newman\nSong "Martini Scorchazio" by ShadowRealmDiceBoys</p>',
+        body:'<p class="description">A work in progress\n\n My final project for DMA 161: Video.  <a href ="https://soundcloud.com/
+        -realm-dice-boys">Click here for more ShadowRealmDiceBoys</a>.\n\n Recording, Director/Editor: Sam Klitsner\nDP: Ky Newman\nSong "Martini Scorchazio" by ShadowRealmDiceBoys</p>',
         press:'<div class="side"><h1>Find me on</h1><li><a href="https://soundcloud.com/shadow-realm-dice-boys" target="_blank">Soundcloud</a></li></div>',
         iframe:'<iframe src="https://player.vimeo.com/video/158510257?title=0&byline=0&portrait=0" width="814" height="453" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     },
